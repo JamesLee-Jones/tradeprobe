9283e66bd79be7adb6943870bd0040e0d525f933
James Lee-Jones <james@lee-jones.co.uk>
2023-06-15 20:07:17 +0100

    Add Event hierarchy
    Add `MarketEvent`, `SignalEvent`, `OrderEvent` and `FillEvent` to the
    event heirarchy.

--- tradeprobe/event_manager.py
+++ tradeprobe/event_manager.py
@@ -3,6 +3,16 @@ from dataclasses import dataclass
 from enum import Enum
 
 
+class SignalType(Enum):
+    LONG = 'LONG'
+    SHORT = 'SHORT'
+
+
+class Direction(Enum):
+    BUY = 'BUY'
+    SELL = 'SELL'
+
+
 class EventManager:
     """The main object used to run a backtest."""
 
@@ -12,28 +22,63 @@ class EventManager:
     def run(self):
         """The main loop for handling events."""
         while not self.event_queue.empty:
-            match self.event_queue.get_nowait().type:
-                case EventType.TICK:
+            match self.event_queue.get_nowait().__class__.__name__:
+                case 'MarketEvent':
                     pass
-                case EventType.FILL:
+                case 'SignalEvent':
                     pass
-                case EventType.ORDER:
+                case 'OrderEvent':
                     pass
-                case EventType.SIGNAL:
+                case 'FillEvent':
                     pass
                 case _:
                     # TODO: Throw error as this is invalid.
                     pass
 
 
-class EventType(Enum):
-    TICK = 1
-    SIGNAL = 2
-    ORDER = 3
-    FILL = 4
-
-
 @dataclass
-class Event:
+class Event(object):
     timestamp: float
-    type: EventType
+
+
+class MarketEvent(Event):
+    """
+    Handle receiving market updates
+    """
+
+
+class SignalEvent(Event):
+    """
+    Handle the sending of a Signal from a strategy object.
+    Used by the portfolio to decide trades.
+    """
+    symbol: str
+    signal_type: SignalType
+
+
+class OrderEvent(Event):
+    """Handle sending an Order."""
+    symbol: str
+    quantity: int
+    direction: Direction
+
+    def __post_init__(self):
+        if self.quantity < 0:
+            raise ValueError("The order quantity must be non-negative.")
+
+    def __str__(self):
+        return f"Order: Symbol={self.symbol}, Quantity={self.quantity}, Direction={self.direction}"
+
+
+class FillEvent(Event):
+    """Encapsulate a filled order from a brokerage."""
+    symbol: str
+    quantity: int
+    direction: Direction
+    fill_price: float
+    commission: float
+
+    def __post_init__(self):
+        if self.quantity < 0:
+            raise ValueError("The order quantity must be non-negative.")
+
